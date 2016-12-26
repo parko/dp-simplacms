90ecfee175b545d4c0db4fcfd30046974762033d
parko <igor@gracelogic.com>
2016-12-26 11:31:26 +0300

    Module added

---
+++ DeltaProcessing/DeltaProcessing.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+require_once('api/Simpla.php');
+
+class DeltaProcessing extends Simpla
+{
+
+	public function checkout_form($order_id, $button_text = null)
+	{
+		if(empty($button_text))
+			$button_text = 'Перейти к оплате';
+
+		$button = '<form method="POST" action="payment/DeltaProcessing/callback.php">
+					<input type="hidden" name="delta_send" value="7"/>
+					<input type="hidden" name="order_id" value="'.$order_id.'"/>
+					<input type="submit" name="submit-button" value="'.$button_text.'"  class="checkout_button">
+					</form>
+					';
+		
+		return $button;
+	}
+}
---
+++ DeltaProcessing/delta_send.php
@@ -0,0 +1,85 @@
+<?php
+
+$order = $simpla->orders->get_order(intval($_REQUEST['order_id']));
+if(empty($order))
+	return;
+ 
+$method = $simpla->payment->get_payment_method(intval($order->payment_method_id));
+if(empty($method))
+	return;
+
+$settings = unserialize($method->settings);
+$payment_currency = $simpla->money->get_currency(intval($method->currency_id));
+
+    $request = array(
+      'checkout' => array(
+        'version' => 2,
+	    "transaction_type"=> "authorization",
+
+		'settings' => array(
+      "success_url"=> $_SERVER["HTTP_REFERER"],
+      "decline_url"=> $_SERVER["HTTP_REFERER"],
+      "fail_url"=> $_SERVER["HTTP_REFERER"],
+      "cancel_url"=> $_SERVER["HTTP_REFERER"],
+      "notification_url"=> $simpla->config->root_url.$_SERVER["REQUEST_URI"],
+      "language"=> "en",
+          'customer_fields' => array(
+            'read_only' => array('email'),
+            'hidden' => array("phone"),
+          ),
+        ),
+        'order' => array(
+      "currency"=> $payment_currency->code,
+      "amount"=> $order->total_price*100,
+      "description"=> "Order ".$order->id." payment"
+        ),
+		'customer' => array(
+      "address"=> $order->address,
+      "name"=> $order->name,
+      "phone"=> $order->phone,
+      "email"=> $order->email,
+        ),
+
+		
+		),
+      );
+
+
+	$request=(object)$request;
+	$request->checkout=(object)($request->checkout);
+	$request->checkout->order=(object)($request->checkout->order);
+	$request->checkout->settings=(object)($request->checkout->settings);
+	$request->checkout->settings->customer_fields=(object)($request->checkout->settings->customer_fields);
+	$request->checkout->customer=(object)($request->checkout->customer);
+
+	$shop_id=$settings['delta_id'];
+	$shop_key=$settings['delta_secret'];
+	$host='https://checkout.deltaprocessing.ru/ctp/api/checkouts';
+
+   $response = d_submit($shop_id, $shop_key, $host, $request);
+   $response = json_decode($response);
+
+	if($response->checkout->redirect_url){
+		header('Location: '.$response->checkout->redirect_url);
+	}
+
+    function d_submit($shop_id, $shop_key, $host, $t_request) {
+
+        $process = curl_init($host);
+        $json = json_encode($t_request);
+
+        if (!empty($t_request)) {
+          curl_setopt($process, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-type: application/json'));
+          curl_setopt($process, CURLOPT_POST, 1);
+          curl_setopt($process, CURLOPT_POSTFIELDS, $json);
+        }
+        curl_setopt($process, CURLOPT_URL, $host);
+        curl_setopt($process, CURLOPT_USERPWD, $shop_id . ":" . $shop_key);
+        curl_setopt($process, CURLOPT_TIMEOUT, 30);
+        curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($process, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($process);
+        curl_close($process);
+
+        return $response;
+    }
